4d32d86b45d1f9f22c07f7faa630ebbc9e3371a3
GarveyJLi <GARVEY.J.LI@GMAIL.COM>
2022-03-15 22:42:50 -0700

    flashcard lab2 updates and README update

---
+++ GarveysFlashcardApp/app/src/main/java/com/example/garveysflashcardapp/AddCardActivity.java
@@ -0,0 +1,59 @@
+package com.example.garveysflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        String current_question = getIntent().getStringExtra("current_question");
+        String current_correct_answer = getIntent().getStringExtra("current_correct_answer");
+        String current_wrong_answer_1 = getIntent().getStringExtra("current_wrong_answer_1");
+        String current_wrong_answer_2 = getIntent().getStringExtra("current_wrong_answer_2");
+
+        EditText question = ((EditText) findViewById(R.id.enter_question_edittext));
+        EditText correct_answer = ((EditText) findViewById(R.id.correct_answer_edittext));
+        EditText incorrect_answer_1 = ((EditText) findViewById(R.id.incorrect_answer_1_edittext));
+        EditText incorrect_answer_2 = ((EditText) findViewById(R.id.incorrect_answer_2_edittext));
+
+        question.setText(current_question);
+        correct_answer.setText(current_correct_answer);
+        incorrect_answer_1.setText(current_wrong_answer_1);
+        incorrect_answer_2.setText(current_wrong_answer_2);
+
+        // User can tap the x icon to switch from AddCardActivity to the MainActivity
+        findViewById(R.id.cancel_new_flashcard_imageview).setOnClickListener(view -> {
+            finish();
+        });
+
+        // User can tap the save icon to switch activities and save the inputted text into the flashcard
+        findViewById(R.id.save_new_flascard_imageview).setOnClickListener(view -> {
+            if (question.getText().toString().equals("") | incorrect_answer_1.getText().toString().equals("") |
+                    incorrect_answer_2.getText().toString().equals("") | correct_answer.getText().toString().equals("")) {
+                Toast.makeText(getApplicationContext(), "Must fill out all fields", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Intent save_card = new Intent();
+                save_card.putExtra("new_question", question.getText().toString());
+                save_card.putExtra("wrong_answer_1", incorrect_answer_1.getText().toString());
+                save_card.putExtra("wrong_answer_2", incorrect_answer_2.getText().toString());
+                save_card.putExtra("correct_answer", correct_answer.getText().toString());
+                setResult(RESULT_OK, save_card);
+                finish();
+            }
+        });
+
+    }
+}
--- GarveysFlashcardApp/app/src/main/java/com/example/garveysflashcardapp/MainActivity.java
+++ GarveysFlashcardApp/app/src/main/java/com/example/garveysflashcardapp/MainActivity.java
@@ -1,137 +1,159 @@
 package com.example.garveysflashcardapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
 
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView flashcard_question = ((TextView) findViewById(R.id.flashcard_question_textview));
+        TextView correct_answer_1 = ((TextView) findViewById(R.id.correct_flashcard_answer_1_textview));
+        TextView correct_answer_2 = ((TextView) findViewById(R.id.correct_flashcard_answer_2_textview));
+        TextView incorrect_answer_1 = ((TextView) findViewById(R.id.incorrect_flashcard_answer_1_textview));
+        TextView incorrect_answer_2 = ((TextView) findViewById(R.id.incorrect_flashcard_answer_2_textview));
+
 
         // User can tap the screen to make the question invisible
-        findViewById(R.id.flashcard_question_textview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // change question from visible to invisible
-                ((TextView) findViewById(R.id.flashcard_question_textview)).setVisibility
-                        (View.INVISIBLE);
-                // change answer form invisible to visible
-                ((TextView) findViewById(R.id.correct_flashcard_answer_2_textview)).setVisibility
-                        (View.VISIBLE);
+        flashcard_question.setOnClickListener(view -> {
+            // change question from visible to invisible
+            flashcard_question.setVisibility(View.INVISIBLE);
+            // change answer from invisible to visible
+            correct_answer_2.setVisibility(View.VISIBLE);
 
-            }
         });
 
-        findViewById(R.id.correct_flashcard_answer_2_textview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // change question from invisible to visible
-                ((TextView) findViewById(R.id.flashcard_question_textview)).setVisibility
-                        (View.VISIBLE);
-                // change answer from visible to invisible
-                ((TextView) findViewById(R.id.correct_flashcard_answer_2_textview)).setVisibility
-                        (View.INVISIBLE);
+        findViewById(R.id.correct_flashcard_answer_2_textview).setOnClickListener(view -> {
+            // change question from invisible to visible
+            flashcard_question.setVisibility(View.VISIBLE);
+            // change answer from visible to invisible
+            correct_answer_2.setVisibility(View.INVISIBLE);
 
-            }
         });
-
 
 
         // User can tap the visibility icon to toggle invisibility for the three answer choices
-        findViewById(R.id.toggle_choices_visibility_visible_imageview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // change answers from visible to invisible
-                ((TextView) findViewById(R.id.correct_flashcard_answer_1_textview)).setVisibility
-                        (View.INVISIBLE);
-                ((TextView) findViewById(R.id.incorrect_flashcard_answer_1_textview)).setVisibility
-                        (View.INVISIBLE);
-                ((TextView) findViewById(R.id.incorrect_flashcard_answer_2_textview)).setVisibility
-                        (View.INVISIBLE);
-                // change "visible" icon from visible to invisible
-                ((ImageView) findViewById(R.id.toggle_choices_visibility_visible_imageview)).setVisibility
-                        (View.INVISIBLE);
-                // change "invisible" icon from invisible to visible
-                ((ImageView) findViewById(R.id.toggle_choices_visibility_invisible_imageview)).setVisibility
-                        (View.VISIBLE);
+        findViewById(R.id.toggle_choices_visibility_visible_imageview).setOnClickListener(view -> {
+            // change answers from visible to invisible
+            correct_answer_1.setVisibility
+                    (View.INVISIBLE);
+            incorrect_answer_1.setVisibility
+                    (View.INVISIBLE);
+            incorrect_answer_2.setVisibility
+                    (View.INVISIBLE);
+            // change "visible" icon from visible to invisible
+            ((ImageView) findViewById(R.id.toggle_choices_visibility_visible_imageview)).setVisibility
+                    (View.INVISIBLE);
+            // change "invisible" icon from invisible to visible
+            ((ImageView) findViewById(R.id.toggle_choices_visibility_invisible_imageview)).setVisibility
+                    (View.VISIBLE);
 
 
-            }
         });
-        findViewById(R.id.toggle_choices_visibility_invisible_imageview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // change answers from visible to invisible
-                ((TextView) findViewById(R.id.correct_flashcard_answer_1_textview)).setVisibility
-                        (View.VISIBLE);
-                ((TextView) findViewById(R.id.incorrect_flashcard_answer_1_textview)).setVisibility
-                        (View.VISIBLE);
-                ((TextView) findViewById(R.id.incorrect_flashcard_answer_2_textview)).setVisibility
-                        (View.VISIBLE);
-                // change "invisible" icon from visible to invisible and "visible" icon from
-                // invisible to visible
-                ((ImageView) findViewById(R.id.toggle_choices_visibility_invisible_imageview)).setVisibility
-                        (View.INVISIBLE);
-                ((ImageView) findViewById(R.id.toggle_choices_visibility_visible_imageview)).setVisibility
-                        (View.VISIBLE);
+        findViewById(R.id.toggle_choices_visibility_invisible_imageview).setOnClickListener(view -> {
+            // change answers from visible to invisible
+            correct_answer_1.setVisibility
+                    (View.VISIBLE);
+            incorrect_answer_1.setVisibility
+                    (View.VISIBLE);
+            incorrect_answer_2.setVisibility
+                    (View.VISIBLE);
+            // change "invisible" icon from visible to invisible and "visible" icon from
+            // invisible to visible
+            ((ImageView) findViewById(R.id.toggle_choices_visibility_invisible_imageview)).setVisibility
+                    (View.INVISIBLE);
+            ((ImageView) findViewById(R.id.toggle_choices_visibility_visible_imageview)).setVisibility
+                    (View.VISIBLE);
 
-            }
         });
 
         // When user taps the incorrect answer, the tapped answer box color changes to red and
         // the correct box color turns green
-        findViewById(R.id.incorrect_flashcard_answer_1_textview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // change textbox color for tapped answer to red
-                ((TextView) findViewById(R.id.incorrect_flashcard_answer_1_textview)).setBackgroundColor
-                        (getResources().getColor(R.color.wrong_red));
-                // change textbox color for correct answer to green
-                ((TextView) findViewById(R.id.correct_flashcard_answer_1_textview)).setBackgroundColor
-                        (getResources().getColor(R.color.right_green));
-            }
+        findViewById(R.id.incorrect_flashcard_answer_1_textview).setOnClickListener(view -> {
+            // change textbox color for tapped answer to red
+            incorrect_answer_1.setBackground(getResources().getDrawable(R.drawable.wrong_card_background));
+            // change textbox color for correct answer to green
+            correct_answer_1.setBackground(getResources().getDrawable(R.drawable.right_card_background));
         });
-        findViewById(R.id.incorrect_flashcard_answer_2_textview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // change textbox color for tapped answer to red
-                ((TextView) findViewById(R.id.incorrect_flashcard_answer_2_textview)).setBackgroundColor
-                        (getResources().getColor(R.color.wrong_red, null));
-                // change textbox color for correct answer to green
-                ((TextView) findViewById(R.id.correct_flashcard_answer_1_textview)).setBackgroundColor
-                        (getResources().getColor(R.color.right_green, null));
-            }
+        findViewById(R.id.incorrect_flashcard_answer_2_textview).setOnClickListener(view -> {
+            // change textbox color for tapped answer to red
+            incorrect_answer_2.setBackground(getResources().getDrawable(R.drawable.wrong_card_background));
+            // change textbox color for correct answer to green
+            correct_answer_1.setBackground(getResources().getDrawable(R.drawable.right_card_background));
         });
 
         // When user taps correct answer, tapped answer box color will change to green
-        findViewById(R.id.correct_flashcard_answer_1_textview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // change textbox color for tapped answer to green
-                ((TextView) findViewById(R.id.correct_flashcard_answer_1_textview)).setBackgroundColor
-                        (getResources().getColor(R.color.right_green, null));
-            }
+        findViewById(R.id.correct_flashcard_answer_1_textview).setOnClickListener(view -> {
+            // change textbox color for tapped answer to green
+            correct_answer_1.setBackground(getResources().getDrawable(R.drawable.right_card_background));
         });
 
         // When the user taps the background, the answer textbox colors reset to their default colors
-        findViewById(R.id.parent).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Change all answer textbox colors to #d1d
-                ((TextView) findViewById(R.id.correct_flashcard_answer_1_textview)).setBackgroundColor
-                        (getResources().getColor(R.color.default_box_pink, null));
-                ((TextView) findViewById(R.id.incorrect_flashcard_answer_1_textview)).setBackgroundColor
-                        (getResources().getColor(R.color.default_box_pink, null));
-                ((TextView) findViewById(R.id.incorrect_flashcard_answer_2_textview)).setBackgroundColor
-                        (getResources().getColor(R.color.default_box_pink, null));
-            }
+        findViewById(R.id.parent).setOnClickListener(view -> {
+            // Change all answer textbox colors to #d1d
+            correct_answer_1.setBackground(getResources().getDrawable(R.drawable.card_background));
+            incorrect_answer_1.setBackground(getResources().getDrawable(R.drawable.card_background));
+            incorrect_answer_2.setBackground(getResources().getDrawable(R.drawable.card_background));
         });
+
+
+        // User can tap the plus icon to switch from MainActivity to the AddCardActivity
+        findViewById(R.id.add_flashcard_button_imageview).setOnClickListener(view -> {
+            Intent add_card = new Intent(MainActivity.this, AddCardActivity.class);
+            MainActivity.this.startActivityForResult(add_card, 100);
+        });
+
+        findViewById(R.id.edit_current_flashcard_imageview).setOnClickListener(view -> {
+            Intent edit_card = new Intent(MainActivity.this, AddCardActivity.class);
+            edit_card.putExtra("current_question", flashcard_question.getText().toString());
+            edit_card.putExtra("current_correct_answer", correct_answer_1.getText().toString());
+            edit_card.putExtra("current_wrong_answer_1", incorrect_answer_1.getText().toString());
+            edit_card.putExtra("current_wrong_answer_2", incorrect_answer_2.getText().toString());
+            MainActivity.this.startActivityForResult(edit_card, 100);
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent save_card) {
+        super.onActivityResult(requestCode, resultCode, save_card);
+        if (requestCode == 100) {
+            if (save_card != null) { // check if there's an Intent
+                String new_question = save_card.getExtras().getString("new_question");
+                String wrong_answer_1 = save_card.getExtras().getString("wrong_answer_1");
+                String wrong_answer_2 = save_card.getExtras().getString("wrong_answer_2");
+                String correct_answer = save_card.getExtras().getString("correct_answer");
+
+                ((TextView) findViewById(R.id.flashcard_question_textview)).setText(new_question);
+                ((TextView) findViewById(R.id.correct_flashcard_answer_1_textview)).setText(correct_answer);
+                ((TextView) findViewById(R.id.correct_flashcard_answer_2_textview)).setText(correct_answer);
+                ((TextView) findViewById(R.id.incorrect_flashcard_answer_1_textview)).setText(wrong_answer_1);
+                ((TextView) findViewById(R.id.incorrect_flashcard_answer_2_textview)).setText(wrong_answer_2);
+
+                Snackbar.make(findViewById(R.id.flashcard_question_textview),
+                        "Flashcard successfully created",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+            else{
+                Snackbar.make(findViewById(R.id.flashcard_question_textview),
+                        "New flashcard cancelled",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }
     }
 }
